c36db9d91470d59adfa34be0b53b43880b2a4698
vishwa-vj21 <iamvishwajith.s@gmail.com>
2025-05-15 03:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (36.44%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,14 +2,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hashmap;
+        unordered_map<int, int> hashmap;  //usage of hashmap
 
         for(int i=0; i<nums.size(); i++){
             int complement = target - nums[i];
-            if(hashmap.count(complement)){
+            if(hashmap.count(complement)){  //checking if it is present inside the hashmap
                 return {hashmap[complement], i};
             }
-            hashmap[nums[i]]=i;
+            hashmap[nums[i]]=i; //if not present, we add them as key value pairs
         }
         return {};
         
