adfb09c5ab488fc6afba0b0cd95849a932153241
vishwa-vj21 <iamvishwajith.s@gmail.com>
2025-05-20 01:58:53 +0530

    Time: 676 ms (76.36%) | Memory: 18.3 MB (63.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,22 +1,20 @@
 class Solution(object):
         def threeSum(self, nums):
-            nums.sort()
+            nums.sort()  #sorting the array first
             res=[]           
             for i,a in enumerate(nums):
-                if i>0 and a==nums[i-1]:
+                if i>0 and a==nums[i-1]: #checking if the current value is equal to the previous value, is yes then skipped
                     continue
-                l, r = i+1, len(nums)-1
+                l, r = i+1, len(nums)-1 
                 while(l<r):                   
                     sum=nums[i]+nums[l]+nums[r]
-                    if(sum==0):
+                    if(sum>0): 
+                        r-=1  #reduce r 
+                    elif(sum<0):
+                        l+=1  #increase l                      
+                    else:
                         res.append([nums[i],nums[l],nums[r]])
                         l+=1
                         while l<r and nums[l]==nums[l-1]:
-                            l+=1
-                    if(sum>0):
-                        r-=1
-                    elif(sum<0):
-                        l+=1                    
-                    
-                        
+                            l+=1  #updating the pointer, when left pointer is updated the right pointer is adjusted in the above conditions
             return res
