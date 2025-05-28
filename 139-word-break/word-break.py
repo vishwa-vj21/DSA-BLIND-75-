eecc54f4c6d8a873eb9fa9e49e7da6bd6f03fc16
vishwa-vj21 <iamvishwajith.s@gmail.com>
2025-05-29 03:51:02 +0530

    Time: 4 ms (72.07%) | Memory: 12.3 MB (99.57%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -6,10 +6,10 @@ class Solution(object):
 
         for i in range(len(s)-1, -1, -1):
             for w in wordDict:
-                if(i+len(w)<=len(s) and s[i:i+len(w)]==w):
-                    dp[i]=dp[i+len(w)]
+                if(i+len(w)<=len(s) and s[i:i+len(w)]==w): #it has to go from i to len(w) and not len(w)+1. and we gotta make sure if we can add the word or it's exceeding the length of the string itself.
+                    dp[i]=dp[i+len(w)]  #if one match is made then we check for the remaing match with this formula
                 if dp[i]:
-                    break
+                    break #no need to check rest if one match is found
         return dp[0]
 
     
