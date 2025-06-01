8a77ef940a14e370dec59ca1a51ef4ffbf55a854
vishwa-vj21 <iamvishwajith.s@gmail.com>
2025-06-02 03:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (83.23%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def rob(self, nums):
         rob1, rob2 = 0, 0
+        #[rob1, rob2, n, n+1, ...]
         for cur_val in nums:
             temp=max(cur_val+rob1, rob2)
             rob1=rob2
