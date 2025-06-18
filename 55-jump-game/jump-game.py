061a3a7156297b6a76c7684fa1ffdefcfac5ebc1
vishwa-vj21 <iamvishwajith.s@gmail.com>
2025-06-18 13:57:21 +0530

    Time: 13 ms (91.52%) | Memory: 13.5 MB (24.81%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def canJump(self, nums):
         goal = len(nums)-1
-        n=len(nums)-1
         for i in range(len(nums)-2, -1, -1):
             if i+nums[i]>=goal:
                 goal=i
