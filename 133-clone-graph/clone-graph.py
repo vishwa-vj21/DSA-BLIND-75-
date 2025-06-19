61cd619edec49d73c038f0963b98ff0bbeaec7b6
vishwa-vj21 <iamvishwajith.s@gmail.com>
2025-06-20 05:15:23 +0530

    Time: 34 ms (96.13%) | Memory: 18.2 MB (39.45%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,11 +1,4 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
+#we make a copy of the old graph that's all
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
