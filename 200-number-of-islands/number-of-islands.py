153387c92b1ef03d1722eb388314a543035c7d69
Vishwajith S <iamvishwajith.s@gmail.com>
2025-07-25 21:07:04 +0530

    Time: 1590 ms (5.00%) | Memory: 31.3 MB (30.35%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
+
+        def bfs(r, c):
+            q=collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+
+            while q:
+                row,col = q.popleft()
+                dir = [[0,1],[1,0],[-1,0],[0,-1]]
+                for dr, dc in dir:
+                    r,c=row+dr, col+dc
+                    if(r in range(rows) and c in range(cols) and grid[r][c]=='1' and (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if (grid[r][c]=='1' and (r,c) not in visit):
+                    bfs(r, c)
+                    islands+=1
+        return islands
+
+        
