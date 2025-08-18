0897c80aa1122464289eb2cb15390462121a1285
Vishwajith S <iamvishwajith.s@gmail.com>
2025-08-18 09:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (83.65%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,9 @@
+
+class Solution(object):
+    def middleNode(self, head):
+        slow, fast= head, head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+        
