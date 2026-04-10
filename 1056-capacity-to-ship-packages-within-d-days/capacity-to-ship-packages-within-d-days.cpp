8b331676336ee609f2572a26b02b7211c9afc034
Vishwajith S <iamvishwajith.s@gmail.com>
2026-04-10 07:37:05 +0530

    Time: 12 ms (51.47%) | Memory: 35 MB (87.25%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -3,31 +3,30 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int low=*max_element(weights.begin(), weights.end());
         int high=accumulate(weights.begin(), weights.end(), 0);
-        int ans=0;
+        int min_res=INT_MAX;
         while(low<=high){
             int mid=(low+high)/2;
-            int res=findDays(weights, mid);
+            int res=capacity_checker(weights, mid);
             if(res<=days){
-                ans=mid;
-                high=mid-1;
+                min_res=mid;  //store capacity
+                high=mid-1;  //check if lesser capacity can be achieved
             }
             else{
                 low=mid+1;
             }
         }
-        return ans;
+        return min_res;
     }
-    int findDays(vector<int>& weights, int capacity){
+    int capacity_checker(vector<int> &weights, int capacity){
+        int bucket=0;
         int days=1;
-        int load=0;
-        for(int i=0; i<weights.size(); i++){
-            if(load+weights[i]>capacity){
-                days+=1;
-                load=weights[i]; // start new day with current item
+        for(int w: weights){
+            bucket+=w;
+            if(bucket>capacity){
+                days++;
+                bucket=w;
             }
-            else{
-                load+=weights[i];
-            }
+            
         }
         return days;
     }
