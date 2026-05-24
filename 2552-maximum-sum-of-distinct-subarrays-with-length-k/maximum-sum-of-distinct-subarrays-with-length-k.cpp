1322bc18d3f5acd1349d11790ddc27f3d93d2a15
Vishwajith S <iamvishwajith.s@gmail.com>
2026-05-24 13:24:33 +0530

    Time: 107 ms (42.98%) | Memory: 98.3 MB (37.22%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -4,17 +4,18 @@ public:
         int start=0;
         long long maxi=0;
         long long maxsum=0;
-        unordered_set<int> hs;
+        unordered_map<int, int> freq;
         
         for(int end=0; end<nums.size(); end++){
-            while(hs.count(nums[end]) || end-start+1>k){
+            maxi+=nums[end];
+            freq[nums[end]]++;
+            while(freq[nums[end]]>1 || end-start+1>k){
                 maxi-=nums[start];
-                hs.erase(nums[start]);
+                freq[nums[start]]--;
                 start++;    
             }
-            maxi+=nums[end];
-            hs.insert(nums[end]);
-            if(hs.size()==k){
+            
+            if(end-start+1==k){
                 maxsum=max(maxi,maxsum);
             }
         }
